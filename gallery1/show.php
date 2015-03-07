92d74193161ec3ba9f584e71f4c16efcc36dc595
harman gulati <singhharman92@yahoo.com>
2015-03-07 10:39:23 +0530

    add colorbox

--- gallery1/show.php
+++ gallery1/show.php
@@ -1,10 +1,19 @@
+<link rel="stylesheet" href="colorbox.css">
+<script src="jquery.js"></script>
+<script src="jquery.colorbox-min.js"></script>
+<script>
+	$(function(){
+		$('a.gallery').colorbox({rel:'gal'});
+	});
+
+</script>
  <?php 
 	error_reporting(E_ALL^E_DEPRECATED);
 	mysql_connect("localhost","root","");
 	mysql_select_db("gallery");
  	$q= mysql_query("select * from images");
  	while($r= mysql_fetch_assoc($q)){
- 		echo '<img src="gallery/'.$r['name'].'" width="50" alt="">';
+ 		echo '<a class="gallery" href="gallery/'.$r['name'].'"><img src="gallery/'.$r['name'].'" width="50" alt=""></a>';
  	}
 
  	?>
